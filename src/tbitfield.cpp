00d6d9be9a6bc6a84161cf7d5589a462f7243c7d
Mike1029 <vodeneevm@mail.ru>
2019-09-20 14:11:15 +0300

    test commit

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -21,10 +21,12 @@ TBitField::~TBitField()
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+	return 0;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+	return 0;
 }
 
 // доступ к битам битового поля
@@ -36,6 +38,7 @@ int TBitField::GetLength(void) const // получить длину (к-во б�
 
 void TBitField::SetBit(const int n) // установить бит
 {
+	
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
@@ -51,6 +54,7 @@ int TBitField::GetBit(const int n) const // получить значение б
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
+	return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
@@ -65,22 +69,29 @@ int TBitField::operator!=(const TBitField &bf) const // сравнение
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
+	return *this;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
+	return *this;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
+	return *this;
 }
 
 // ввод/вывод
 
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
+	return istr;
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+	return  ostr;
 }
+
+//VODENEEVBET
